768746ba3e60b91639e08c7ad148eccf5acaaa58
cyrille <cyrilleotieno7@gmail.com>
2024-04-01 13:55:03 +0300

    hide authentication api

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,7 +2,7 @@ import { configs } from '@configs/index'
 import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
-  fastify.get('/', async function (_request, _reply) {
+  fastify.get('/', { schema: { hide: true } }, async function (_request, _reply) {
     return { root: true, status: `${configs.appname} server is healthy` }
   })
 }
