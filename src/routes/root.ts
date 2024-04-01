310d013832f65813f869a27431c6f28ffb5bbf58
cyrille <cyrilleotieno7@gmail.com>
2024-04-01 14:54:24 +0300

    fix sms sent

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -3,7 +3,6 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, _opts): Promise<void> => {
   fastify.get('/', { schema: { hide: true } }, async function (_request, _reply) {
-    await fastify.messaging.sendSms('0708073370', 'helpa')
     return { root: true, status: `${configs.appname} server is healthy` }
   })
 }
