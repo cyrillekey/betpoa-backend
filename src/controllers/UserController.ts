d558ef13c73bf6a3e71b8db878339d3b61e3c62f
cyrille <cyrilleotieno7@gmail.com>
2024-04-02 09:33:08 +0300

    add sentry

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -17,63 +17,72 @@ export class UserController extends BaseController {
     return otp
   }
   async signupCustomer() {
-    const body: ISignUpBody = this.req.body as ISignUpBody
-    const phone = this.app.helpers.formatPhoneNumber(body.phone)
-    if (!this.app.helpers.validatePhoneNumber(phone)) {
-      return this.res.status(400).send({
-        success: false,
-        id: null,
-        message: 'Invalid Phone number provided',
-      })
-    }
-    const exists = await this.app.prisma.user
-      .findFirstOrThrow({
-        where: {
-          phone: phone,
-        },
-        select: {
-          id: true,
-        },
-      })
-      .then((response) => response?.id)
-      .catch(() => null)
-    if (exists)
-      return this.res.status(400).send(<IDefaultResponse>{
-        success: false,
-        id: null,
-        message: 'Failed! Phone number already used',
-      })
-    const salt = genSaltSync(10)
-    const password = await hash(body.password, salt)
-    const user = await this.app.prisma.user.create({
-      data: {
-        password,
-        phone,
-        phoneValidated: false,
-        profile: {
-          create: {
-            fname: 'Customer',
-            lname: 'Customer',
+    try {
+      const body: ISignUpBody = this.req.body as ISignUpBody
+      const phone = this.app.helpers.formatPhoneNumber(body.phone)
+      if (!this.app.helpers.validatePhoneNumber(phone)) {
+        return this.res.status(400).send({
+          success: false,
+          id: null,
+          message: 'Invalid Phone number provided',
+        })
+      }
+      const exists = await this.app.prisma.user
+        .findFirstOrThrow({
+          where: {
+            phone: phone,
+          },
+          select: {
+            id: true,
+          },
+        })
+        .then((response) => response?.id)
+        .catch(() => null)
+      if (exists)
+        return this.res.status(400).send(<IDefaultResponse>{
+          success: false,
+          id: null,
+          message: 'Failed! Phone number already used',
+        })
+      const salt = genSaltSync(10)
+      const password = await hash(body.password, salt)
+      const user = await this.app.prisma.user.create({
+        data: {
+          password,
+          phone,
+          phoneValidated: false,
+          profile: {
+            create: {
+              fname: 'Customer',
+              lname: 'Customer',
+            },
           },
         },
-      },
-    })
-    const jwtUser = {
-      id: user?.id,
-      phone: user?.phone,
-      phoneValidated: user?.phoneValidated,
-      profileId: user?.profileId,
-      role: user?.role,
-    }
-    const token = this.app.helpers.jwt.sign(jwtUser)
+      })
+      const jwtUser = {
+        id: user?.id,
+        phone: user?.phone,
+        phoneValidated: user?.phoneValidated,
+        profileId: user?.profileId,
+        role: user?.role,
+      }
+      const token = this.app.helpers.jwt.sign(jwtUser)
 
-    return this.res.status(201).send(<IDefaultResponse>{
-      id: user?.id,
-      success: true,
-      message: 'Success! User Created',
-      token: token,
-      user: jwtUser,
-    })
+      return this.res.status(201).send(<IDefaultResponse>{
+        id: user?.id,
+        success: true,
+        message: 'Success! User Created',
+        token: token,
+        user: jwtUser,
+      })
+    } catch (error) {
+      this.app.Sentry.captureException(error)
+      return this.res.status(500).send(<IDefaultResponse>{
+        id: null,
+        success: false,
+        message: 'Failed! Something went wrong please try again',
+      })
+    }
   }
   async loginUser() {
     try {
@@ -113,6 +122,7 @@ export class UserController extends BaseController {
         user: jwtUser,
       })
     } catch (error) {
+      this.app.Sentry.captureException(error)
       return this.res.status(500).send(<IDefaultResponse>{
         id: null,
         success: false,
